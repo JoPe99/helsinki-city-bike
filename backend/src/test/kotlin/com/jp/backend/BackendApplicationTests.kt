2783cc465b4ae27913f96e95679d807e5400044d
Peni <joo67@hotmail.com>
2022-12-20 13:23:28 +0200

    Kotlin backend initialized

---
+++ backend/src/test/kotlin/com/jp/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jp.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
