d5b67319ab02864552bca74058044715a2f13efe
Peni <joo67@hotmail.com>
2023-01-06 06:25:55 +0200

    Created TimePicker

---
+++ frontend/src/components/insert/TimePicker.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    :nudge-right="60"
+    :return-value.sync="time"
+    transition="scale-transition"
+    offset-y
+    max-width="290px"
+    min-width="290px"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        v-model="time"
+        :label="label"
+        readonly
+        outlined
+        v-bind="attrs"
+        v-on="on"
+        color="highlight"
+      ></v-text-field>
+    </template>
+    <v-time-picker
+      v-if="menu"
+      v-model="time"
+      full-width
+      use-seconds
+      format="24hr"
+      @click:second="save(time)"
+    ></v-time-picker>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TimePicker",
+  props: ["id", "defaultTime", "label"],
+
+  mounted() {
+    this.time = this.defaultTime;
+  },
+
+  data: () => ({
+    menu: false,
+    time: "" as string,
+  }),
+
+  methods: {
+    save(time: string) {
+      let menuElement = this.$refs.menu;
+      (menuElement as Vue & { save: (v: string) => boolean }).save(time);
+
+      this.$emit("newData", { id: this.$props.id, time: time });
+      this.time = time;
+    },
+  },
+});
+</script>
